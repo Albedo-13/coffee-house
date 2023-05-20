32e12ebd343adf1c4dbe8bc4aba2eb39df882a74
Pavel Prokopenya <pavel12minsk@mail.ru>
2023-05-20 17:43:32 +0300

    refactor: remove code duplication

--- src/components/ourBest/OurBest.js
+++ src/components/ourBest/OurBest.js
@@ -3,41 +3,27 @@ import './ourBest.scss';
 const OurBest = (props) => {
   const { data } = props;
 
+  const content = data.slice(3).map(item => {
+    return (
+      <a href="#" className="our-best-item" key={item.id}>
+        <div>
+          <img className="our-best-item-img"
+            src={item.img}
+            alt="product"
+            draggable="false" />
+          <div className="our-best-item-name">{item.name}</div>
+          <div className="our-best-item-price">{item.price}$</div>
+        </div>
+      </a>
+    );
+  });
+  
   return (
     <section className="our-best">
       <div className="container">
         <h2 className="our-best-header">Our best</h2>
         <div className="our-best-wrapper">
-          <a href="#" className="our-best-item">
-            <div>
-              <img className="our-best-item-img"
-                src={data[0].img}
-                alt="product"
-                draggable="false" />
-              <div className="our-best-item-name">{data[0].name}</div>
-              <div className="our-best-item-price">{data[0].price}$</div>
-            </div>
-          </a>
-          <a href="#" className="our-best-item">
-            <div>
-              <img className="our-best-item-img"
-                src={data[0].img}
-                alt="product"
-                draggable="false" />
-              <div className="our-best-item-name">{data[0].name}</div>
-              <div className="our-best-item-price">{data[0].price}$</div>
-            </div>
-          </a>
-          <a href="#" className="our-best-item">
-            <div>
-              <img className="our-best-item-img"
-                src={data[0].img}
-                alt="product"
-                draggable="false" />
-              <div className="our-best-item-name">{data[0].name}</div>
-              <div className="our-best-item-price">{data[0].price}$</div>
-            </div>
-          </a>
+          {content}
         </div>
       </div>
     </section>
