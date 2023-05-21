a383b686c3d515ae76f18dbbca4ef8e24fb15870
Pavel Prokopenya <pavel12minsk@mail.ru>
2023-05-21 18:52:50 +0300

    refactor: delete useless files & fix spinner anim

--- src/components/pages/MainPage/MainPage.js
+++ src/components/pages/MainPage/MainPage.js
@@ -2,10 +2,12 @@ import Header from '../../header/Header';
 import AboutUs from '../../aboutUs/AboutUs';
 import OurBest from '../../ourBest/OurBest';
 import Footer from '../../footer/Footer';
+import Spinner from '../../spinner/Spinner';
 
 const MainPage = (props) => {
   return (
     <>
+      <Spinner />
       <Header currPage={'MainPage'} />
       <AboutUs />
       <OurBest data={props.data} />
