f40b823ec598170aeee6f3ec5fc32bdb2a5f0905
Pavel Prokopenya <pavel12minsk@mail.ru>
2023-05-21 18:53:34 +0300

    refactor: remove test spinner

--- src/components/pages/MainPage/MainPage.js
+++ src/components/pages/MainPage/MainPage.js
@@ -2,12 +2,10 @@ import Header from '../../header/Header';
 import AboutUs from '../../aboutUs/AboutUs';
 import OurBest from '../../ourBest/OurBest';
 import Footer from '../../footer/Footer';
-import Spinner from '../../spinner/Spinner';
 
 const MainPage = (props) => {
   return (
     <>
-      <Spinner />
       <Header currPage={'MainPage'} />
       <AboutUs />
       <OurBest data={props.data} />
